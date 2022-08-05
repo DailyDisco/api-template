41ba9ff03b4076016ebbfcb420ab40a136407315
DailyDisco <actual.mensch0r@icloud.com>
2022-08-05 03:32:13 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', data.name)
+
+let name = document.querySelector('.name')
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data.name + '' + data.weight)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
